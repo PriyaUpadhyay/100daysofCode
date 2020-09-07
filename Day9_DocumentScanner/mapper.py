79adc44520be7183579969f103c95cfd48dec8d0
PriyaUpadhyay <68734957+PriyaUpadhyay@users.noreply.github.com>
2020-09-07 22:07:21 +0530

    Update mapper.py

--- Day9_DocumentScanner/mapper.py
+++ Day9_DocumentScanner/mapper.py
@@ -1,15 +1,23 @@
 import numpy as np
 
 def mapp(h):
+    # initialzie a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
     h = h.reshape((4,2))
-    hnew = np.zeros((4,2),dtype = np.float32)
+    rect = np.zeros((4,2),dtype = np.float32)
 
     add = h.sum(1)
-    hnew[0] = h[np.argmin(add)]
-    hnew[2] = h[np.argmax(add)]
+    # the top-left point will have the smallest sum, whereas the bottom-right point will have the largest sum
+    rect[0] = h[np.argmin(add)]
+    rect[2] = h[np.argmax(add)]
+    
+    # now, compute the difference between the points, the top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
 
     diff = np.diff(h,axis = 1)
-    hnew[1] = h[np.argmin(diff)]
-    hnew[3] = h[np.argmax(diff)]
+    rect[1] = h[np.argmin(diff)]
+    rect[3] = h[np.argmax(diff)]
 
-    return hnew
+    return rect
